5c860c279927635d92851e3e6a1787d867915642
jose.carlos.mendoza.prego <josecarlos.mendoza@mimacom.com>
2020-05-22 15:14:52 +0200

    Experimenting with Event Registry

--- src/main/java/com/canchitodev/example/springflowableintegration/SpringFlowableIntegrationApplication.java
+++ src/main/java/com/canchitodev/example/springflowableintegration/SpringFlowableIntegrationApplication.java
@@ -45,21 +45,21 @@ public class SpringFlowableIntegrationApplication {
 		SpringApplication.run(SpringFlowableIntegrationApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner init(final RepositoryService repositoryService,
-								  final RuntimeService runtimeService,
-								  final TaskService taskService) {
-
-		return new CommandLineRunner() {
-			@Override
-			public void run(String... strings) throws Exception {
-				System.out.println("Number of process definitions : "
-						+ repositoryService.createProcessDefinitionQuery().count());
-				System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
-				runtimeService.startProcessInstanceByKey("oneTaskProcess");
-				System.out.println("Number of tasks after process start: "
-						+ taskService.createTaskQuery().count());
-			}
-		};
-	}
+//	@Bean
+//	public CommandLineRunner init(final RepositoryService repositoryService,
+//								  final RuntimeService runtimeService,
+//								  final TaskService taskService) {
+//
+//		return new CommandLineRunner() {
+//			@Override
+//			public void run(String... strings) throws Exception {
+//				System.out.println("Number of process definitions : "
+//						+ repositoryService.createProcessDefinitionQuery().count());
+//				System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+//				runtimeService.startProcessInstanceByKey("oneTaskProcess");
+//				System.out.println("Number of tasks after process start: "
+//						+ taskService.createTaskQuery().count());
+//			}
+//		};
+//	}
 }
